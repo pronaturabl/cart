5e4e0ff36fbb919f3d7182ea619cf51529063f2f
thomas <thomas@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-01 10:36:49 +0000

    Fixed PHP.
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/cart@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,5 +1,11 @@
-<?php?>
-<?xml version="1.0" encoding="UTF-8"?>
+<?php
+
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+$themeroot='http://r-forge.r-project.org/themes/rforge/';
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
