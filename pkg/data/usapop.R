5daca472e50e5c723b27e366ed1b27ef33b3f9f3
thomas <thomas@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-06 20:45:59 +0000

    Rename object to usapop.
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/cart@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/data/usapop.R
+++ pkg/data/usapop.R
@@ -1,3 +1,3 @@
 ## read U.S.A. population data
-uspop <- read.table("usapop.txt", header = TRUE, sep = "\t",
+usapop <- read.table("usapop.txt", header = TRUE, sep = "\t",
                     row.names = 1)
