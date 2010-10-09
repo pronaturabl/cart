726f036bfd6ac3e5fdd4e3fab2e3e6c289f39de3
thomas <thomas@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-09 11:11:14 +0000

    Proper documentation of dataset.
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/cart@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/cart.R
+++ pkg/R/cart.R
@@ -205,3 +205,22 @@ interpolate <- function(xy, xgrid, ygrid) {
   return(mat)
   
 }
+
+
+
+##'  Population data of the USA.
+##'
+##' @name usapop
+##' @docType data
+##' @format A data frame with 52 observations on the following variable.
+##' \describe{
+##' \item{\code{population}}{a numeric vector: estimated number of people living
+##' in a specific U.S. state in 2009}
+##' }
+##' The state names are given in the data frame row names.
+##' @source \link{http://en.wikipedia.org/wiki/List_of_U.S._States_by_population},
+## retrieved 29 September 2010 19:04 UTC, Page Version ID: 383596724
+##' @examples
+##' data(usapop)
+##' str(usapop)
+##' @keywords data
